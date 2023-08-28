ae5a391bddf876359fd9f6edef1ec144341059e8
Ramón A. Mediavilla <ramon.mediavilla@ironia.tech>
2023-08-29 01:06:47 +0200

    Set environment variables from a file

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-// reference: https://www.npmjs.com/package/dotenv
+const fs = require('node:fs')
 
 function config (options = {}) {
-  // your code here
+  const filePath = options?.path ?? './.env'
+
+  if (fs.existsSync(filePath)) {
+    const text = fs.readFileSync(filePath, 'utf-8')
+    const lines = text.split('\n')
+
+    lines.forEach(line => {
+      const parts = line.split('=')
+
+      if (parts.length > 0) {
+        const key = parts[0]
+        const value = parts[1]?.replace(/["']/g, '') ?? ''
+        process.env[key] = value
+      }
+    })
+  }
 }
 
 module.exports = { config }
